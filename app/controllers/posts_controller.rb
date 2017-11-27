240068f3e9726461f95944c7b8cf12dfb1fbdeb7
natalietate <martin.nat@gmail.com>
2017-11-27 11:21:12 -0500

    add ability to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,28 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  # create method is always a post method, so it doesn't need a view
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def get_post
+  @post = Post.find(params[:id])
+end
 end
