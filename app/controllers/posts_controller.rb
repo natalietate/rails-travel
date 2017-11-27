efa7140cf040d78ffc5b66be20f6b2e530dff5d3
natalietate <martin.nat@gmail.com>
2017-11-27 12:36:32 -0500

    ability to upload file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :avatar)
   end
 
   def get_post
